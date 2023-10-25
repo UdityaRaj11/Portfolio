8cefb42e55ac78ced8b35d308504ba004efb944c
Uditya Raj <95217170+UdityaRaj11@users.noreply.github.com>
2023-10-25 14:59:08 +0000

    made required changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Uditya Raj",
+  title: "Developer & Innovator",
+  email: "uditya.123.raj@gmail.com",
+  gitHub: "https://github.com/UdityaRaj11",
+  instagram: "https://www.instagram.com/uditya_raj_001",
+  linkedIn: "https://www.linkedin.com/in/uditya-raj-b6152b224",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/UdityaRaj11",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/coder-desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,25 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Nodelink2.0",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Dynamic social networking application focused on personal growth for learners and growth seekers.",
+    url: "https://github.com/UdityaRaj11/Nodelink2.0",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "SkimLit",
+    description: "An NLP model to make reading medical abstracts easier.",
+    url: "",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "ThriftyU",
+    description: "A Smart E-wallet for students to spend being thrifty.",
+    url: "https://github.com/UdityaRaj11/ThriftyU",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "MyBooka",
+    description: "A simple Book Renting application",
+    url: "https://github.com/UdityaRaj11/MyBooka",
   },
 ];
 
@@ -64,7 +61,12 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideInLeft",
+            }}
             alt={imageAltText}
           />
         </div>
