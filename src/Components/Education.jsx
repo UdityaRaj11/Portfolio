eade0c2cff7a29e0bd1e13c357683e3075253b36
Uditya Raj <95217170+UdityaRaj11@users.noreply.github.com>
2023-10-25 18:35:48 +0000

    prettified

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,32 +4,32 @@ import image from "../images/education.jpg";
 const imageAltText = "desktop with books and laptop";
 
 const Education = () => {
-    return (
-        <section id="education">
-            <h2 style={{ textAlign: "center" }}>Education</h2>
-            <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem", left: "40%"}}>
-            <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-                    <img
-                        src={image}
-                        style={{
-                        height: "90%",
-                        width: "100%",
-                        objectFit: "cover",
-                        animation: "1s ease-out 0s 1 slideInLeft",
-                        }}
-                        alt={imageAltText}
-                    />
-                </div> 
-                <div className="edcontainer">
-                    <div style={{ maxWidth: "70%", alignSelf: "center" }} className="box">
-                        <h3>BTech in Computer Science and Engineering</h3>
-                        <h4>2021 - 2025</h4>
-                        <h4>Institute of Technical Education and Research, IN</h4>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem", left: "40%" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideInLeft",
+            }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="edcontainer">
+          <div style={{ maxWidth: "70%", alignSelf: "center" }} className="box">
+            <h3>BTech in Computer Science and Engineering</h3>
+            <h4>2021 - 2025</h4>
+            <h4>Institute of Technical Education and Research, IN</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Education;
