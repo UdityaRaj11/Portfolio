24d918126cce350addac054a15a92c60d348d26f
Uditya Raj <95217170+UdityaRaj11@users.noreply.github.com>
2023-10-25 15:10:18 +0000

    Added new Section

---
+++ src/Components/Education.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Education = () => {
+    return (
+        <section id="education" className="light">
+            <h2>Education</h2>
+            <div className="educationCard">
+                <div className="card-content">
+                    <h3>BTech in Computer Science and Engineering</h3>
+                    <h4>2021 - 2025</h4>
+                    <h4>Institute of Technical Education and Research</h4>
+                    <p>CGPA: 7.8</p>
+                </div>   
+            </div>
+        </section>
+    )
+}
+
+export default Education;
